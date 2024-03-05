c5a019bddbdddd989f8360503eadbd2da3d441eb
edd1212 <edwinalwin@gmail.com>
2024-03-05 16:26:06 +0530

    Fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
+
+let accordion = [
+  {
+    id: 1,
+    head: "Heading",
+    status: false,
+    content:
+      "Master modern React from beginner to advanced! Context API, React Query, Redux Toolkit, Tailwind, advanced patterns",
+  },
+  {
+    id: 2,
+    head: "Heading",
+    status: true,
+    content:
+      "React from beginner to advanced! Context API, React Query, Redux Toolkit, Tailwind, advanced patterns",
+  },
+  {
+    id: 3,
+    head: "Heading",
+    status: false,
+    content:
+      "Context API, React Query, Redux Toolkit Master modern React from beginner to advanced! Master modern , Tailwind, advanced patterns",
+  },
+];
+
+// function Header() {
+//   return (
+//     <header>
+//       <div>Home</div>
+//       <div>Services</div>
+//       <div>Contact</div>
+//     </header>
+//   );
+// }
+// function Button({ color, txt, children }) {
+//   return (
+//     <button data-txt={txt} style={{ color: color }}>
+//       {children}
+//     </button>
+//   );
+// }
+
+function Accordion({ data }) {
+  const [accordionStatus, setStatus] = useState(1);
+  return (
+    <div className="accordion-wrapper">
+      {data.map((el) => (
+        <AccordionItem
+          curStatus={accordionStatus}
+          onSetStatus={setStatus}
+          key={el.id}
+          heading={el.head}
+          content={el.content}
+          status={el.status}
+          id={el.id}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ heading, content, curStatus, id, onSetStatus }) {
+  let toOpen = id === curStatus;
+  function updateStatus() {
+    onSetStatus(toOpen ? null : id)
+  }
+  return (
+    <div className="accordion-box">
+      <div className="head" onClick={updateStatus}>
+        <div>
+          {heading} {id}
+        </div>
+        <span className="icon">{toOpen === true ? "-" : "+"}</span>
+      </div>
+      {toOpen && <div className="content">{content}</div>}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Header />
+      <Button txt={"Left"} color={"red"}>
+        👈 Left
+      </Button>
+      <Button txt={"Left"} color={"gold"}>
+        Right 👉
+      </Button> */}
+
+      <Accordion data={accordion} />
+    </>
   );
 }
 
